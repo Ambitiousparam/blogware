173f33b8959c16237decccf468165c80c0b06cd2
parampreet singh <parampreetsingh1092@gmail.com>
2023-04-18 20:44:28 +0530

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 
 import './App.css';
+import CreatePost from "./pages/CreatePost";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import {useState} from "react";
+
 import {BrowserRouter as Router,Routes,Route,Link} from "react-router-dom"
 
 function App() {
-  return ( 
+  const [isAuth, setIsAuth] = useState(false);
+return (
    <Router>
+    <nav>
+      <Link to ="/">Home</Link>
+      <Link to ="/createpost"> Create Post </Link>
+      <Link to ="/login"> Login </Link>
+
+
+    </nav>
     <Routes>
       <Route path = "/" element ={<Home />} />
+      <Route path = "/createPost" element ={<CreatePost/>} />
+      <Route path = "/login" element ={<Login/>} />
     </Routes>
    </Router>
   );
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import {auth,provider} from "../firebase"
+import { signInWithPopup } from "firebase/auth";
 
 function login() {
+  const signinwithgoogle =()=>{
+    signInWithPopup(auth,provider).then((result)=>{
+      
+    })
+  };
+
   return (
-    <div>login</div>
+    <div className='loginpage'>
+      <p>Sign In With Google to Continue</p>
+    <button className='googlebtn'>Sign In With Google</button>
+    </div>
   )
 }
 
